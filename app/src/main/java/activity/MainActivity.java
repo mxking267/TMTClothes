93c6cc20497821ba31416235832cc5bdda99d011
Thạch Bùi Ngọc <2001216134@hufi.edu.vn>
2023-04-22 11:32:56 +0700

    push

--- app/src/main/java/activity/MainActivity.java
+++ app/src/main/java/activity/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -52,5 +53,6 @@ public class MainActivity extends AppCompatActivity {
         }
         return true;
     };
+
 
 }
